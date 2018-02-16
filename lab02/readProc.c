07f2c2a161c7ffce9d35fe53335d2ff8933f8992
Valalala <walter.rasmussen@maine.edu>
2018-02-16 10:37:59 -0500

    Lab02 started, barely.

---
+++ lab02/readProc.c
@@ -0,0 +1,13 @@
+// Boom Johnson - Walter Rasmussen - Spring 2018
+// helloWorld.c - Prints "Hello World!"
+
+#include<stdio.h>
+#inculde<signal.h>
+int main(int argc, char *argz[]) {
+	signal( SIGALRM, ALRMhandler);
+	File *fp;
+	
+	
+	
+	return 0;
+}
