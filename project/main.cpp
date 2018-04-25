aed6732b721d6e891ab089af7a2c455881eb5b20
Valalala <walter.rasmussen@maine.edu>
2018-04-25 21:32:19 +0000

    Weights data.

--- project/main.cpp
+++ project/main.cpp
@@ -203,12 +203,14 @@ int main(int argc, const char * argv[]) {
         vector<float> pred = sigmoid(dot(X, W, 4, 4, 1 ) );
         vector<float> pred_error = y - pred;        
         vector<float> pred_delta = pred_error * sigmoid_d(pred);        
-        vector<float> W_delta = dot(transpose( &X[0], 4, 4 ), pred_delta, 4, 4, 1);        
-        W = W + W_delta;
+        vector<float> W_delta = dot(transpose( &X[0], 4, 4 ), pred_delta, 4, 4, 1);
+	
+	W = W + W_delta;
         
         //if (i == EPOCHS-1){
-            print ( pred, 4, 1 );
+            //print ( pred, 4, 1 );
         //};
+	print(W, 4, 1);
     };
 
     return 0;
